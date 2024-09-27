efb7284d94ae7e0d45120032d71e3c586c50c3b9
Deep Patel <patel_deep1217@outlook.com>
2024-09-27 18:27:33 -0400

    add reset and global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,18 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import './styles.css'
 import { cookies } from "next/headers";
 import {  DARK_TOKENS, LIGHT_TOKENS } from "@/constants";
 
 import ThemeToggle from "@/components/ThemeToggle";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { Inter } from 'next/font/google'
+ 
+export const inter = Inter({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -37,13 +34,14 @@ export default function RootLayout({
 
   return (
     <html lang="en" data-color-theme={theme}
+    className={inter.className}
     style={
       theme === 'light'
         ? LIGHT_TOKENS
         : DARK_TOKENS
     }>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <ThemeToggle initialTheme={theme}/>
+      <body>
+   
         {children}
       </body>
     </html>
